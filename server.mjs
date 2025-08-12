6fa534ef22ede4d723a6048fd3c9dcafc2036503
marl26-namo <marlnestprnamondwe@gmail.com>
2025-08-12 22:35:21 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'https://nmc1.rf.gd/',
   credentials: true
 }));
 app.use(express.json());
